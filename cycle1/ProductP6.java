05ea611a6ffec51e506e0c6dd58d4e3a19118cbf
Sandrasanthosh339 <74653003+Sandrasanthosh339@users.noreply.github.com>
2021-06-09 13:58:48 +0530

    09/06/2021

--- cycle1/ProductP6.java
+++ cycle1/ProductP6.java
@@ -1,83 +1,72 @@
 
-import java.util.*;
-
-public class ProductP6
+import java.util.Scanner;
+class ProductP6
 {
 int pcode;
 String pname;
-int price;
-
-ProductP6()
+float price;
+void Record(int cod,String nam,float prc)
 {
-pcode=0;
-pname=null;
-price=0;
+pcode=cod;
+pname=nam;
+price=prc;
 }
-
-
- 
-public static void cal(int a,int b,int c)
+void PriceLow(float pr1,float pr2,float pr3,String nm1,String nm2,String nm3)
 {
-int p1=a;
-int p2=b;
-int p3=c;
-float lowest;
 
-if(p1<p2)
+if(pr1<pr2 && pr1<pr3)
 {
-if(p3<p1) 
-{
-lowest = p3;
-
-} 
-else 
-{
-lowest = p1;
-
+System.out.println("The Product With Lowest Price Is:"+nm1);
+System.out.println("The Lowest Price Of the Product is: "+pr1);
 }
-} 
-else 
+else if(pr2<pr1 && pr2<pr3) 
 {
-if(p2<p3) 
+System.out.println("The Product With Lowest Price Is:"+nm2);
+System.out.println("The Lowest Price Of the Product is: "+pr2);
+}
+else if(pr3<pr2 && pr3<pr1)
 {
-lowest = p2;
-
-} 
+System.out.println("The Product With Lowest Price Is:"+nm3);
+System.out.println("The Lowest Price Of the Product is: "+pr3);
+}
 else
 {
-lowest = p3;
-
+System.out.println("all products have same price");
 }
-}
-
-System.out.println("The lowest price among the 3 Product is : "+lowest);
 }
 public static void main(String args[])
 {
 Scanner s = new Scanner(System.in);
+
+
+System.out.print("Enter the Product 1 Code : ");
+int cod1 =s.nextInt();
+System.out.print("Enter the Product 1 Name : ");
+String name1 = s.next();
+System.out.print("Enter the Product 1 Price : ");
+float price1=s.nextFloat();
+
+System.out.print("Enter the product 2 Code : ");
+int cod2 =s.nextInt();
+System.out.print("Enter the Product 2 Name : ");
+String name2 = s.next();
+System.out.print("Enter the Product 2 Price : ");
+float price2 = s.nextFloat();
+
+System.out.print("Enter the Product 3 Code : ");
+int cod3 =s.nextInt();
+System.out.print("Enter the Product 3 Name : ");
+String name3 = s.next();
+System.out.print("Enter the Product 3 Price : ");
+float price3 = s.nextFloat();
+
 ProductP6 p1= new ProductP6();
 ProductP6 p2= new ProductP6();
 ProductP6 p3= new ProductP6();
+p1.Record(cod1,name1,price1);
+p2.Record(cod2,name2,price2);
+p3.Record(cod3,name3,price3);
 
-System.out.print("Enter the Product 1 Code : ");
-p1.pcode =s.nextInt();
-System.out.print("Enter the Product 1 Name : ");
-p1.pname = s.next();
-System.out.print("Enter the Product 1 Price : ");
-p1.price=s.nextInt();
-System.out.print("Enter the product 2 Code : ");
-p2.pcode =s.nextInt();
-System.out.print("Enter the Product 2 Name : ");
-p2.pname = s.next();
-System.out.print("Enter the Product 2 Price : ");
-p2.price = s.nextInt();
-System.out.print("Enter the Product 3 Code : ");
-p3.pcode =s.nextInt();
-System.out.print("Enter the Product 3 Name : ");
-p3.pname = s.next();
-System.out.print("Enter the Product 3 Price : ");
-p3.price = s.nextInt();
-
-ProductP6.cal(p1.price,p2.price,p3.price);
+p1.PriceLow(price1,price2,price3,name1,name2,name3);
 }
 }
